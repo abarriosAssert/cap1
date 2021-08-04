1ef8ecc46ad4fbf2d95be5d50e59af8b229bbc9f
Andrés Barrios <abarrios@assertsolutions.com>
2021-08-04 11:15:12 -0500

    a

--- src/main/java/com/assertsolutions/capacitaciones/model/Empleado.java
+++ src/main/java/com/assertsolutions/capacitaciones/model/Empleado.java
@@ -7,6 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
 public class Empleado implements Serializable {
 	
